714d9b059c7ad290af31a4a238dd5423299d70cd
Michael Grube <michael.grube@gmail.com>
2014-06-29 12:15:50 -0400

    Changed awareness.py to two files: awareness_alert and awareness_verify.
    Alert is meant to run periodically and alert people on the list.
    
    Verify is meant to periodically check retweeted statuses of people on the list and see if those match up with alerted statuses.

---
+++ awareness_verify.py
@@ -0,0 +1,48 @@
+from twython import Twython
+import random
+import pickle
+from time import sleep
+import MySQLdb as mdb
+
+#MySQL Time!
+conn = mdb.connect('localhost', 'awareness', 'changeme', 'awareness')
+cur = conn.cursor()
+
+
+def getAlertedTweets():
+	alertids = list()
+	numentries = cur.execute("SELECT Tweetid FROM Alerts;")
+	count = 0
+	while count < numentries:
+		alertids.append(cur.fetchone())
+		count += 1
+	return alertids
+
+
+#### This stuff connects us to twitter.com
+
+APP_KEY = 'not'
+APP_SECRET = 'for'
+OAUTH_TOKEN = 'your'
+OAUTH_TOKEN_SECRET = 'eyes'
+
+twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+peopleWhoCare = twitter.get_list_members(slug='iraq-car-bomb-admin-list',owner_screen_name='weaware')
+
+alertedtweets = getAlertedTweets()
+
+for p in peopleWhoCare['users']:
+	timeline = twitter.get_user_timeline(screen_name=p['screen_name'], count=200, exclude_replies=True)
+	retweets = list()
+	for t in timeline:
+		try:
+			if t['retweeted_status'] is not None:
+                		retweets.append(t)
+		except KeyError:
+			print "Not a retweet"
+	for r in retweets:
+		if long(r['rewteeted_status']['id']) in alertedtweets:
+			try:
+				#cur.execute()
+				print "INSERT INTO Verifications (OPScreenname, TweetText, Tweetid, VerifierScreenName, VerifyingTweetID) VALUES ('%s', '%s', %s, '%s', %s);" % (r['retweeted_status']['user']['screen_name'], r['retweeted_status']['text'], r['retweeted_status']['id'], r['user']['screen_name'], r['id'])
