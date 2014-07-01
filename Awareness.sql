99efdeac7e04b8f3d51595a44e9ec7591d09b24f
Michael Grube <michael.grube@gmail.com>
2014-07-01 11:07:21 -0400

    Whoops, added two URL columns to Verifications.

--- Awareness.sql
+++ Awareness.sql
@@ -5,6 +5,6 @@ CREATE DATABASE awareness;
 CREATE USER 'awareness'@'localhost' IDENTIFIED BY  'changeme';
 GRANT ALL ON awareness.* TO 'awareness'@'localhost';
 
-CREATE TABLE awareness.Verifications (OPScreenName varchar(100),  TweetText varchar(500), Tweetid bigint, URL varchar(1000), VerifierScreenName varchar(100), VerifyingTweetID bigint, URL varchar(1000), PRIMARY KEY (Tweetid, VerifierScreenName));
+CREATE TABLE awareness.Verifications (OPScreenName varchar(100),  TweetText varchar(500), Tweetid bigint, URL varchar(1000), VerifierScreenName varchar(100), VerifyingTweetID bigint, PRIMARY KEY (Tweetid, VerifierScreenName));
 
 CREATE TABLE awareness.Alerts (OPScreenName varchar(100),  TweetText varchar(500), Tweetid bigint, URL varchar(1000), PRIMARY KEY (Tweetid));
