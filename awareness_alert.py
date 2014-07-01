050ed7eafc732eaa1faa4d66f62b2bab77576ec2
Michael Grube <michael.grube@gmail.com>
2014-07-01 09:18:54 -0400

    We don't want our own tweets!

--- awareness_alert.py
+++ awareness_alert.py
@@ -10,10 +10,10 @@ cur = conn.cursor()
 
 #### This stuff connects us to twitter.com
 
-APP_KEY = 'not'
-APP_SECRET = 'for'
-OAUTH_TOKEN = 'your'
-OAUTH_TOKEN_SECRET = 'eyes'
+APP_KEY = 'nope'
+APP_SECRET = 'nope'
+OAUTH_TOKEN = 'nope'
+OAUTH_TOKEN_SECRET = 'nope'
 
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
@@ -25,6 +25,17 @@ peopleWhoCare = twitter.get_list_members(slug='iraq-car-bomb-admin-list',owner_s
 
 rand = random.choice(results['statuses'])
 
+#This is a temporary hacked solution. Ideally we just insert the IDs of all of our own tweets into a table.
+#However I'd like a little more time to think about the structure 
+#Disgustin' hack. MAKE ME PRETTIER.
+ourtweets =  twitter.get_user_timeline(screen_name='weaware', count=200)
+ourids = list()
+
+for t in ourtweets:
+	ourids.append(t['id'])
+
+while rand['id'] in ourids:
+	rand = random.choice(results['statuses'])
 
 try:
 	cur.execute("INSERT INTO Alerts (OPScreenName, TweetText, Tweetid) VALUES ('%s', '%s', %s);" % (rand['user']['screen_name'], rand['text'], rand['id']))
