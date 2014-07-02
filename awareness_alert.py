318bfea18e76eed4a5336d502d75c0fd6c30ad27
Bilal Ghalib <bg@bilalghalib.com>
2014-07-01 17:45:53 -0700

    Removing duplicates
    removing duplicates and only polling the last 12 hours worth of tweets
    :)
    Please use four spaces in python rather than tabs, it’s been
    problematic, pythontidy cleans things up nicely :)

--- awareness_alert.py
+++ awareness_alert.py
@@ -1,78 +1,88 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from twython import Twython
 import random
+import pickle
+import datetime
+from datetime import timedelta
+from time import sleep
 import MySQLdb as mdb
 import urllib2
 
-#Follow a URL to get the actual story URL
 def followURL(url):
-	request = urllib2.Request(url)
-	request.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:23.0) Gecko/20131011 Firefox/23.0')
-	opener = urllib2.build_opener()
-	f = opener.open(request)
-	return f.url
+    request = urllib2.Request(url)
+    request.add_header('User-Agent',
+                       'Mozilla/5.0 (Windows NT 6.1; rv:23.0) Gecko/20131011 Firefox/23.0'
+                       )
+    opener = urllib2.build_opener()
+    f = opener.open(request)
+    return f.url
 
 
-#MySQL Time!
-conn = mdb.connect('localhost', 'awareness', 'changeme', 'awareness')
-cur = conn.cursor()
+conn = mdb.connect('localhost', 'changeme', 'changeme', 'changeme')
+cur = conn.cursor() 
 
-#### This stuff connects us to twitter.com
 
-APP_KEY = ''
-APP_SECRET = ''
-OAUTH_TOKEN = ''
-OAUTH_TOKEN_SECRET = ''
+APP_KEY = 'changeme'
+APP_SECRET = 'changeme'
+OAUTH_TOKEN = 'changemechangeme'
+OAUTH_TOKEN_SECRET = 'changeme'
 
 
 urlcount = cur.execute('SELECT URL FROM Verifications;')
 urls = list()
 i = 0
 while i < urlcount:
-	urls.append(cur.fetchone()[0])
-	i += 1
-
+    urls.append(cur.fetchone()[0])
+    i += 1
 
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
-results = twitter.search(q='iraq car bomb', count=200)
+halfADayAgo = datetime.datetime.now() - timedelta(hours=1212)
+halfADayAgo = halfADayAgo.strftime('%Y-%m-%d')
 
-peopleWhoCare = twitter.get_list_members(slug='iraq-car-bomb-admin-list',owner_screen_name='weaware')
+results = twitter.search(q='iraq car bomb', since=halfADayAgo,
+                         count=200)
 
-#rand = random.choice(results['statuses'])
+peopleWhoCare = twitter.get_list_members(slug='iraq-car-bomb-admin-list'
+        , owner_screen_name='weaware')
 
-#This is a temporary hacked solution. Ideally we just insert the IDs of all of our own tweets into a table.
-#However I'd like a little more time to think about the structure 
-#Disgustin' hack. MAKE ME PRETTIER.
-ourtweets =  twitter.get_user_timeline(screen_name='weaware', count=200)
+ourtweets = twitter.get_user_timeline(screen_name='weaware', count=200)
 ourids = list()
 
 for t in ourtweets:
-	ourids.append(t['id'])
-
-#We don't want our own tweets and we aren't interested if there is not a URL
+    ourids.append(t['id'])
 
 acceptable = list()
 
 for r in results['statuses']:
-	if r['id'] not in ourids and len(r['entities']['urls']) != 0:
-		try:
-			if followURL(r['entities']['urls'][0]['expanded_url']) not in urls:
-				acceptable.append(r)
-		except urllib2.HTTPError, e:
-			print r['entities']['urls'][0]['expanded_url']
-			print e
+    if r['id'] not in ourids and len(r['entities']['urls']) != 0:
+        try:
+            if followURL(r['entities']['urls'][0]['expanded_url'])\
+                 not in urls:
+                acceptable.append(r)
+        except urllib2.HTTPError, e:
+            print r['entities']['urls'][0]['expanded_url']
+            print e
 
 if len(acceptable) != 0:
-
-	rand = random.choice(acceptable)
-	print rand['entities']['urls'][0]['expanded_url']
-
-	try:
-		cur.execute("INSERT INTO Alerts (OPScreenName, TweetText, Tweetid, URL) VALUES ('%s', '%s', %s, '%s');" % (rand['user']['screen_name'], rand['text'], rand['id'], followURL(rand['entities']['urls'][0]['expanded_url'])))
-		conn.commit() 
-		for p in peopleWhoCare['users']:
-			url = "https://twitter.com/%s/status/%s/" % (rand['user']['screen_name'], rand['id'])
-			message = "@" + p['screen_name'] + " Please retweet if this is a valid event: " + url
-			twitter.update_status(status=message)
-	except mdb.Error, e:
-		print e
+    for p in peopleWhoCare['users']:
+        rand = random.choice(acceptable)
+        print rand['entities']['urls'][0]['expanded_url']
+        try:
+            cur.execute("INSERT INTO Alerts (OPScreenName, TweetText, Tweetid, URL) VALUES ('%s', '%s', %s, '%s');"
+            % (rand['user']['screen_name'], rand['text'],
+            rand['id'], followURL(rand['entities']['urls'
+            ][0]['expanded_url'])))
+            conn.commit()
+            url = 'https://twitter.com/%s/status/%s/' % (rand['user'
+                    ]['screen_name'], rand['id'])
+            message = '@' + p['screen_name']\
+                 + ' Please retweet if this is a valid event: ' + url
+            try:
+                twitter.update_status(status=message)
+            except:
+                print "probably a dupe"
+        except mdb.Error, e:
+            print e
--- awareness_verify.py
+++ awareness_verify.py
@@ -1,58 +1,80 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from twython import Twython
 import random
-import urllib2, httplib
+import urllib2
+import httplib
 import MySQLdb as mdb
 
-#MySQL Time!
-conn = mdb.connect('localhost', 'awareness', 'changeme', 'awareness')
-cur = conn.cursor()
+conn = mdb.connect('localhost', 'changeme', 'changeme', 'changeme')
+cur = conn.cursor() 
 
 def followURL(url):
-        request = urllib2.Request(url)
-        request.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:23.0) Gecko/20131011 Firefox/23.0')
-        opener = urllib2.build_opener()
-        f = opener.open(request)
-        return f.url
+    request = urllib2.Request(url)
+    request.add_header('User-Agent',
+                       'Mozilla/5.0 (Windows NT 6.1; rv:23.0) Gecko/20131011 Firefox/23.0'
+                       )
+    opener = urllib2.build_opener()
+    f = opener.open(request)
+    return f.url
 
 
 def getAlertedTweets():
-	alertids = list()
-	numentries = cur.execute("SELECT Tweetid FROM Alerts;")
-	count = 0
-	while count < numentries:
-		alertids.append(cur.fetchone()[0])
-		count += 1
-	return alertids
+    alertids = list()
+    numentries = \
+        cur.execute('SELECT Tweetid FROM Alerts where isPublished is NULL;'
+                    )
+    count = 0
+    while count < numentries:
+        alertids.append(cur.fetchone()[0])
+        count += 1
+    return alertids
 
 
-#### This stuff connects us to twitter.com
 
-APP_KEY = ''
-APP_SECRET = ''
-OAUTH_TOKEN = ''
-OAUTH_TOKEN_SECRET = ''
-
+APP_KEY = 'changeme'
+APP_SECRET = 'changeme'
+OAUTH_TOKEN = 'changemechangeme'
+OAUTH_TOKEN_SECRET = 'changeme'
 
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
-peopleWhoCare = twitter.get_list_members(slug='iraq-car-bomb-admin-list',owner_screen_name='weaware')
+peopleWhoCare = twitter.get_list_members(slug='iraq-car-bomb-admin-list'
+        , owner_screen_name='weaware')
 
 alertedtweets = getAlertedTweets()
 
 for p in peopleWhoCare['users']:
-	timeline = twitter.get_user_timeline(screen_name=p['screen_name'], count=200, exclude_replies=True)
-	retweets = list()
-	for t in timeline:
-		try:
-			if t['retweeted_status'] is not None and len(t['retweeted_status']['entities']['urls']) != 0:
-                		retweets.append(t)
-		except KeyError:
-			print "Not a retweet"
-	for r in retweets:
-		if long(r['retweeted_status']['id']) in alertedtweets:
-			followURL(r['retweeted_status']['entities']['urls'][0]['expanded_url'])
-			try:
-				cur.execute("INSERT INTO Verifications (OPScreenname, TweetText, Tweetid, VerifierScreenName, VerifyingTweetID, URL) VALUES ('%s', '%s', %s, '%s', %s, '%s');" % (r['retweeted_status']['user']['screen_name'], r['retweeted_status']['text'], r['retweeted_status']['id'], r['user']['screen_name'], r['id'], followURL(r['retweeted_status']['entities']['urls'][0]['expanded_url'])))
-				conn.commit()
-			except mdb.Error, e:
-				print 'Something went wrong. Probably a primary key violation, which is Okey dokey!'
+    timeline = twitter.get_user_timeline(screen_name=p['screen_name'],
+            count=200, exclude_replies=True)
+    retweets = list()
+    for t in timeline:
+        try:
+            if t['retweeted_status'] is not None:
+                retweets.append(t)
+        except KeyError:
+            print 'Not a retweet'
+    for r in retweets:
+        if long(r['retweeted_status']['id']) in alertedtweets:
+            try:
+                tweetPerson = r['user']['screen_name']
+                idOut = r['retweeted_status']['id']
+                URLOut = 'https://twitter.com/%s/status/%s/'\
+                     % (tweetPerson, idOut)
+                messageToMember2 = '.@' + tweetPerson\
+                     + ' has verified this attack: ' + URLOut
+                print messageToMember2[0:139]
+                try:
+                    twitter.update_status(status=messageToMember2[0:139])
+                    cur.execute("INSERT INTO Verifications (OPScreenname, TweetText, Tweetid, VerifierScreenName, VerifyingTweetID) VALUES ('%s', '%s', %s, '%s', %s);"
+                                 % (r['retweeted_status']['user'
+                                ]['screen_name'], r['retweeted_status'
+                                ]['text'], r['retweeted_status']['id'],
+                                r['user']['screen_name'], r['id']))
+                    cur.execute("UPDATE Alerts SET isPublished=1 WHERE Tweetid=r['retweeted_status']['id']"
+                                )
+                    conn.commit()
+                except:
+                    print 'poooplicate'
+            except mdb.Error, e:
+                print 'Something went wrong. Probably a primary key violation, which is Okey dokey!'
